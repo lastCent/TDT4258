51feb8970af66cda9e1c5c00c68c2ba4c45feb19
lastCent <richard.bman@gmail.com>
2017-09-07 21:30:47 +0200

    Added gpio and LED testing.

--- ex1/ex1.s
+++ ex1/ex1.s
@@ -82,7 +82,42 @@
 	      .type   _reset, %function
         .thumb_func
 _reset: 
-	      b .  // do nothing
+cmu_base_addr:
+	.long CMU_BASE
+
+	//Enable GPIO
+	CMU_BASE = 0x400c8000
+	CMU_HFPERCLKEN0 = 0x044
+	CMU_HFPERCLKEN0_GPIO = 13
+	LDR	R1,	cmu_base_addr
+	LDR	R2,	[R1, #CMU_HFPERCLKEN0]
+	MOV	R3,	#1
+	LSL	R3,	R3,	#CMU_HFPERCLKEN0_GPIO
+	ORR	R2,	R2,	R3
+	STR	R2,	[R1, #CMU_HFPERCLKEN0]
+ 
+	// Write 0x2 to GPIO_PA_CTRL
+	LDR	R0,	=GPIO_PA_BASE
+	LDR	R1,	=GPIO_CTRL
+	ADD	R2,	R1,	R0
+	MOV	R3,	0x2
+	STR	R3,	[R2]
+	// Set pins 8-15 to output
+	LDR	R0,	=GPIO_PA_BASE
+	LDR 	R1,	=GPIO_MODEH
+	ADD	R2,	R1,	R0
+	MOV	R3,	0x55555555
+	STR	R3,	[R2]
+	//Turn pins on
+	LDR	R0,	=GPIO_PA_BASE
+	LDR	R1,	=GPIO_DOUT
+	ADD	R2,	R1,	R0
+	MOV	R3,	0b11110000000
+	STR	R3,	[R2]
+	// Enable interrupt vector
+	LDR	R0,	=ISER0
+	MOV	R1, 	0x802
+	b .  // do nothing
 	
 	/////////////////////////////////////////////////////////////////////////////
 	//
