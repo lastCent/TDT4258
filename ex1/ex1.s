0ac5177741362a1c13bf7ba4118c0642d1de5bd4
Dodib <duong_mnb@hotmail.com>
2017-09-07 14:19:05 +0200

    hello

--- ex1/ex1.s
+++ ex1/ex1.s
@@ -1,457 +1,135 @@
         .syntax unified
-
-
-
 	
-
-
-
 	      .include "efm32gg.s"
 
-
-
-
-
-
-
 	/////////////////////////////////////////////////////////////////////////////
-
-
-
 	//
-
-
-
   // Exception vector table
-
-
-
   // This table contains addresses for all exception handlers
-
-
-
 	//
-
-
-
 	/////////////////////////////////////////////////////////////////////////////
-
-
-
 	
-
-
-
         .section .vectors
-
-
-
 	
-
-
-
 	      .long   stack_top               /* Top of Stack                 */
-
-
-
 	      .long   _reset                  /* Reset Handler                */
-
-
-
 	      .long   dummy_handler           /* NMI Handler                  */
-
-
-
 	      .long   dummy_handler           /* Hard Fault Handler           */
-
-
-
 	      .long   dummy_handler           /* MPU Fault Handler            */
-
-
-
 	      .long   dummy_handler           /* Bus Fault Handler            */
-
-
-
 	      .long   dummy_handler           /* Usage Fault Handler          */
-
-
-
 	      .long   dummy_handler           /* Reserved                     */
-
-
-
 	      .long   dummy_handler           /* Reserved                     */
-
-
-
 	      .long   dummy_handler           /* Reserved                     */
-
-
-
 	      .long   dummy_handler           /* Reserved                     */
-
-
-
 	      .long   dummy_handler           /* SVCall Handler               */
-
-
-
 	      .long   dummy_handler           /* Debug Monitor Handler        */
-
-
-
 	      .long   dummy_handler           /* Reserved                     */
-
-
-
 	      .long   dummy_handler           /* PendSV Handler               */
-
-
-
 	      .long   dummy_handler           /* SysTick Handler              */
 
-
-
-
-
-
-
 	      /* External Interrupts */
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   gpio_handler            /* GPIO even handler */
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   gpio_handler            /* GPIO odd handler */
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
 	      .long   dummy_handler
-
-
-
-
-
-
 
 	      .section .text
 
-
-
-
-
-CMU_BASE = 0X400C800
-
-CMU_HFPERCLKEN0 = 0X044
-
-CMU_HFPERCLKEN0_GPIO = 13
-
-
-
-LDR R1, CMU_BASE_ADDR
-
-LDR R2, [R1, #CMU_HFPERCLKEN0]
-
-MOV R3, #1
-
-LSL R3, R3, #CMU_HFPERCLKEN0_GPIO
-
-ORR R2, R2, R3
-
-STR R2, [R1, #CMU_HFPERCLKEN0]
-
-LDR R4, =GPIO_PA_BASE
-LDR R5, =GPIO_CTRL
-MOV R6, #0X2
-STR R6, [R4, R5]
-
-LDR R4, =GPIO_PA_BASE
-LDR R5, =GPIO_MODEH
-MOV R6, #0X55555555
-STR R6, [R4, R5]
-
-LDR R4, =GPIO_PA_BASE
-LDR R5, =GPIO_DOUT
-MOV R6, #0b0100000000
-STR R6, [R4, R5]
-
-
-
-
-
-CMU_BASE_ADDR:
-
-	.LONG CMU_BASE
-
-
-
-
-
-
-
 	/////////////////////////////////////////////////////////////////////////////
-
-
-
 	//
-
-
-
 	// Reset handler
-
-
-
   // The CPU will start executing here after a reset
-
-
-
 	//
-
-
-
 	/////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
 
 	      .globl  _reset
-
-
-
 	      .type   _reset, %function
-
-
-
         .thumb_func
-
-
-
 _reset: 
-
-
-
-	      b .  // do nothing
-
-
-
+cmu_base_addr:
+	.long CMU_BASE
+
+	CMU_BASE = 0x400c8000
+	CMU_HFPERCLKEN0 = 0x044
+	CMU_HFPERCLKEN0_GPIO = 13
+	LDR R1, cmu_base_addr
+	LDR R2, [R1, #CMU_HFPERCLKEN0]
+	MOV R3, #1
+	LSL R3, R3, #CMU_HFPERCLKEN0_GPIO
+	ORR R2, R2, R3
+	STR R2, [R1, #CMU_HFPERCLKEN0]
+
+	LDR R1, =GPIO_PA_BASE
+	LDR R2, =GPIO_CTRL
+	ADD R4, R2, R1
+	MOV R3, 0x2
+	STR R3, [R4]
+
+	LDR R1, =GPIO_PA_BASE
+	LDR R2, =GPIO_MODEH
+	ADD R4, R2, R1
+	MOV R3, 0x55555555
+	STR R3, [R4]
+
+	LDR R1,	=GPIO_PA_BASE
+	LDR R2,	=GPIO_DOUT
+	ADD R4,	R2, R1
+	MOV R3, 0b1111110000000
+	STR R3,	[R4]
 	
-
-
-
+	b .  // do nothing
+	
 	/////////////////////////////////////////////////////////////////////////////
-
-
-
 	//
-
-
-
   // GPIO handler
-
-
-
   // The CPU will jump here when there is a GPIO interrupt
-
-
-
 	//
-
-
-
 	/////////////////////////////////////////////////////////////////////////////
-
-
-
 	
-
-
-
         .thumb_func
-
-
-
 gpio_handler:  
 
-
-
-
-
-
-
 	      b .  // do nothing
-
-
-
 	
-
-
-
 	/////////////////////////////////////////////////////////////////////////////
-
-
-
 	
-
-
-
         .thumb_func
-
-
-
 dummy_handler:  
-
-
-
         b .  // do nothing
 
